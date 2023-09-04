2bc29c27d926039b12d303b0206ba89a495836be
kang <guswjd9994@duksung.ac.kr>
2023-09-04 23:41:08 +0900

    2047-신문 헤드라인

---
+++ SW/2047.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner a = new Scanner(System.in);
+		String str = a.next();
+		System.out.println(str.toUpperCase());
+	}
+}
