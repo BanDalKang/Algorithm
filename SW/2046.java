88177c038d6fe08d800727767d8002b8c5932d66
= <guswjd9994@duksung.ac.kr>
2023-09-05 23:59:05 +0900

    2046-스탬프 찍기

---
+++ SW/2046.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		
+        for(int i=0; i<n; i++){
+        	System.out.print("#");
+        }
+	}
+}
