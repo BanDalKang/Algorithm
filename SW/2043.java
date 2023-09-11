6ef213f4156e748c2110df07e34e3fc1c207b5f1
kang <guswjd9994@duksung.ac.kr>
2023-09-11 23:11:16 +0900

    2043-서랍의 비밀번호

---
+++ SW/2043.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		
+		Scanner sc = new Scanner(System.in);
+		int p, k;
+		p=sc.nextInt();
+        k=sc.nextInt();
+
+		System.out.print(p-k+1);
+	}
+}
