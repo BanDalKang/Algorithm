f31b457c28e0df144e955e984d7a878e1761f5c1
= <guswjd9994@duksung.ac.kr>
2023-09-23 21:49:11 +0900

    1966-숫자를 정렬하자

---
+++ SW/D2/1966.java
@@ -0,0 +1,26 @@
+package D2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int t = 1; t <= T; t++) {
+            int N = sc.nextInt();
+            int[] arr = new int[N];
+            for (int i = 0; i < N; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            Arrays.sort(arr);
+
+            System.out.print("#" + t + " ");
+            for (int i = 0; i < N; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
