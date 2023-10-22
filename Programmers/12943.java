933462ed43a2aa3202e3bf0449cedd220a7118f5
= <guswjd9994@duksung.ac.kr>
2023-10-22 10:22:09 +0900

    12943-콜라츠 추측

---
+++ Programmers/12943.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int num) {
+        long number = (long) num; // int 범위를 넘어가는 경우를 대비하여 long으로 변환
+
+        for (int i = 0; i < 500; i++) {
+            if (number == 1) {
+                return i; // 1이 되면 반복 횟수를 반환
+            }
+
+            number = (number % 2 == 0) ? number / 2 : number * 3 + 1;
+        }
+
+        return -1; // 500번 반복해도 1이 되지 않으면 -1 반환
+    }
+}
