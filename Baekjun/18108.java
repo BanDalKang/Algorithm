e4cb2e1125b0adf397a61ee94e7972e53db55166
= <guswjd9994@duksung.ac.kr>
2023-11-28 10:46:29 +0900

    18108-1998년생인 내가 태국에서는 2541년생?!

---
+++ Baekjun/18108.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine()) - 543;
+        StringBuilder sb = new StringBuilder();
+        sb.append(t);        
+        System.out.print(sb);
+    }
+}
