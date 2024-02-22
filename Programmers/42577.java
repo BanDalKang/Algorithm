62bc747e30b193669d74e2eed05260bbd6fb39dd
kang <guswjd9994@duksung.ac.kr>
2024-02-22 10:45:19 +0900

    42577-전화번호 목록

---
+++ Programmers/42577.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        HashMap<String, Integer> phone_numberMap = new HashMap<>();
+
+        // 전화번호부를 해시맵에 저장
+        for (String phone_number : phone_book) {
+            phone_numberMap.put(phone_number, 1);
+        }
+
+        // 각 전화번호의 접두어가 해시맵에 있는지 확인
+        for (String phoneNumber : phone_book) {
+            for (int i = 1; i < phoneNumber.length(); i++) {
+                if (phone_numberMap.containsKey(phoneNumber.substring(0, i))) {
+                    return false; // 접두어가 있으면 false 반환
+                }
+            }
+        }
+
+        // 접두어가 없는 경우 true 반환
+        return true;
+    }
+}
