9549a6c323215ec24008896b1c2069b0ed021896
kang <guswjd9994@duksung.ac.kr>
2024-03-24 10:49:19 +0900

    131128-숫자 짝꿍

---
+++ Programmers/131128.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String solution(String X, String Y) {
+        int[] xCount = new int[10];
+        int[] yCount = new int[10];
+
+        for (char c : X.toCharArray()) {
+            xCount[c - '0']++;
+        }
+
+        for (char c : Y.toCharArray()) {
+            yCount[c - '0']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 9; i >= 0; i--) { 
+            int minCount = Math.min(xCount[i], yCount[i]);
+            for (int j = 0; j < minCount; j++) {
+                sb.append(i);
+            }
+        }
+
+        String answer = sb.toString();
+        if (answer.isEmpty()) {
+            return "-1";
+        } else if (answer.charAt(0) == '0') {
+            return "0";
+        }
+
+        return answer;
+    }
+} 
